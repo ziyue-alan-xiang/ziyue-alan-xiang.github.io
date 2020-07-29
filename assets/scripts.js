ee74c6043ff83a12be9ef92c1c3917ba52bf5ffe
Alan Xiang <xziyue@syr.edu>
2020-07-29 01:59:17 -0400

    update

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,3 +1,40 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+
+
+function generate_toc() {
+  var navSelector = "#toc";
+  var $myNav = $(navSelector);
+  $("body").css({"position":"relative"});
+
+  Toc.init({
+      $nav : $myNav,
+      $scope: $("h2")
+  });
+
+  $("body").scrollspy({
+    target: navSelector
+  });
+
+  $('[data-spy="scroll"]').each(function () {
+  var $spy = $(this).scrollspy('refresh')
+  })
+}
+
+var _entity_map = {
+  '&': '&amp;',
+  '<': '&lt;',
+  '>': '&gt;',
+  '"': '&quot;',
+  "'": '&#39;',
+  '/': '&#x2F;',
+  '`': '&#x60;',
+  '=': '&#x3D;'
+};
+
+function escape_html (string) {
+  return String(string).replace(/[&<>"'`=\/]/g, function (s) {
+    return _entity_map[s];
+  });
+}
